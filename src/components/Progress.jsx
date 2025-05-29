fba72604f3eff5724fa5efe5a63333f0dbdbc7a8
Manel <manel.delahoz@mataro.epiaedu.cat>
2025-05-29 13:05:35 +0200

    pacients

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -2,7 +2,7 @@ export const Progress = ({ progress, setProgress }) => {
   return progress.map((progressInformation, i) => (
     <tr key={i} className="hover:bg-gray-50">
       <td className="px-4 py-2" style={{ textAlign: "center" }}>
-        {progressInformation.power_ups}
+        {JSON.stringify(progressInformation.power_ups)}
       </td>
       <td className="px-4 py-2" style={{ textAlign: "center" }}>
         {progressInformation.monedas}
